3b2c48c62c069bb1d26b51a0394d04bdbe8aa6d8
Stella M Thomas <anuraga2021@mca.ajce.in>
2021-06-22 13:20:25 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
     $d=date('d-m-Y');
-    echo "<br><b><font color='green'><h3>AUTOMATIC REFRESH AFTER EVERY 15 SECONDS :)</h3></font></b><br><a><button class='btn btn-primary'>Reload</button></a><br><h4>Showing results of date : ".$d." + 7 Days Details</h4><hr>";
+    echo "<br><b><font color='green'><h3>CoWin Vaccination Availability - Kollam District</h3></font></b><br><a><button class='btn btn-primary'>Reload</button></a><br><h4>Showing results of date : ".$d." + 7 Days Details</h4><hr>";
 
 
   //header( "refresh:10;url=index.php" );
@@ -92,6 +92,19 @@ curl_close($curl);
 
     <title>Hello, world!</title>
   </head>
+  <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/60d175137f4b000ac038d6f9/1f8p32lkn';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
   <body>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>

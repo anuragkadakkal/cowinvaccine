cf44005218015397f00d457b00caac31e47b3fe8
Stella M Thomas <anuraga2021@mca.ajce.in>
2021-06-22 13:17:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
     $d=date('d-m-Y');
-    echo "<b><font color='green'><h2>AUTOMATIC REFRESH AFTER EVERY 15 SECONDS :)</h2></font></b><br><h3>Showing results of date : ".$d." + 7 Days Details</h3><hr>";
+    echo "<br><b><font color='green'><h2>AUTOMATIC REFRESH AFTER EVERY 15 SECONDS :)</h2></font></b><br><button class='btn btn-primary'>Reload</button><h3>Showing results of date : ".$d." + 7 Days Details</h3><hr>";
 
 
   header( "refresh:10;url=index.php" );
@@ -83,6 +83,18 @@ curl_close($curl);
 <!doctype html>
 <html lang="en">
   <head>
-    <title>CoWin Vaccination Checker - Anurag Kadakkal</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+     <title>CoWin Vaccination Checker - Anurag Kadakkal</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
   </head>
+  <body>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
 </html>
